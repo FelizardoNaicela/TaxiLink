0f604087a79b0f5a195f92b09e1a7cd568635e3f
Felizardo Naicela <felizardonaicela11@gimail.com>
2026-06-04 01:54:35 +0200

    Atualiza CORS para producao

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
 app.enableCors({
-  origin: 'http://localhost:5174',
+  origin: [
+    'http://localhost:5174',
+    'https://taxi-link-kappa.vercel.app',
+  ],
 });
 
 app.useGlobalPipes(
